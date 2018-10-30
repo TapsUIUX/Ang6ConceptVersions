4aecf23dbe43a376f1d20ac10c1adc7ba18eceb1
TapsUIUX <25495356+TapsUIUX@users.noreply.github.com>
2018-10-30 06:15:03 -0400

    Conent Child
    getting access to the content of the projected compnent via conaten child. we ve accessed the contet of check box components that emmiting a event, and we get acces to it thru viewcontent child and sunscribed to that in the form component . dependiong on that we swiched on and off a message

--- src/app/components/shared/form/form.component.ts
+++ src/app/components/shared/form/form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit ,EventEmitter, Output} from '@angular/core';
+import { Component, OnInit ,EventEmitter, Output , ContentChild, AfterContentInit} from '@angular/core';
 import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import {CheckBoxComponent} from '../check-box/check-box.component';
  
 
 @Component({
@@ -7,20 +8,32 @@ import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms'
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit,AfterContentInit {
   //declare
   user:FormGroup;
   pre:boolean=false;
+  showMessage:boolean;
   @Output() form_e = new EventEmitter();
+
+  @ContentChild(CheckBoxComponent) checkbox : CheckBoxComponent;
+
   constructor(private _fb:FormBuilder) { }
   
   ngOnInit() {
     this.user = this._fb.group({
-      
+
         email: ['', Validators.required],
         password: ['', Validators.required]
       
     });
+  }
+
+  ngAfterContentInit(){
+  if(this.checkbox){
+    this.checkbox.checked_e.subscribe((checked:boolean)=>{
+      this.showMessage = checked;
+    })
+  }
   }
 
   formSubmit(){
